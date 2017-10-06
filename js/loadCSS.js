a561c130d0c0feaed13d849b2bdce86eff6cc050
Nicolas DELAUNEY <niko-d@live.fr>
2017-10-06 20:00:56 +0100

    :sparkles: :art: Add white theme
    ... but it's odd.

--- js/loadCSS.js
+++ js/loadCSS.js
@@ -24,8 +24,14 @@ fontawesome.setAttribute("rel","stylesheet")
 fontawesome.setAttribute("href","../assets/font-awesome/css/font-awesome.css")
 document.head.appendChild(fontawesome)
 
+const jsonfile = require('jsonfile')
+
 // Personnal CSS
 let myCSS = document.createElement("link")
 myCSS.setAttribute("rel","stylesheet")
-myCSS.setAttribute("href","../assets/master.css")
+if(jsonfile.readFileSync(__dirname+'/../settings.json').theme == 'light'){
+  myCSS.setAttribute("href","../assets/light.css");
+} else {
+  myCSS.setAttribute("href","../assets/dark.css");
+}
 document.head.appendChild(myCSS)
