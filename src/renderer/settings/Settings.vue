20ef89b356735c2909e8d7d82d6073e7ebc0997c
Meikel Philipp <meikel-lp@web.de>
2018-09-02 15:41:57 +0200

    [ADD] SASS instead of plain CSS

--- src/renderer/settings/Settings.vue
+++ src/renderer/settings/Settings.vue
@@ -112,11 +112,3 @@ export default {
   }
 }
 </script>
-
-<style lang="css">
-/* CSS */
-/* Work only in dev. */
-@import url('~@/assets/css/settings.css');
-@import url('~@/assets/css/light.css');
-@import url('~@/assets/css/dark.css');
-</style>
