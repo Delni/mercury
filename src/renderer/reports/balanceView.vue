bae12f6b16d21fce33e7076b8ceb2c89af17bc9d
Meikel Philipp <meikellp@users.noreply.github.com>
2018-09-17 22:56:56 +0200

    [FIX] Icons in balance report

--- src/renderer/reports/balanceView.vue
+++ src/renderer/reports/balanceView.vue
@@ -4,7 +4,7 @@
     <div class="field has-addons">
       <div class="control">
         <a class="button is-primary is-tag">
-          <font-awesome-icon fa="calendar"/>
+          <font-awesome-icon icon="calendar"/>
         </a>
       </div>
       <div class="control select is-primary">
@@ -20,7 +20,7 @@
       <div class="control field has-addons column">
         <div class="control">
           <a class="button is-primary is-tag">
-            <font-awesome-icon fa="calendar-plus"/>
+            <font-awesome-icon icon="calendar-plus"/>
           </a>
         </div>
         <div class="control" style="width: 10vw">
@@ -36,7 +36,7 @@
       <div class="control field has-addons column">
         <div class="control">
           <a class="button is-primary is-tag">
-            <font-awesome-icon fa="calendar-minus"/>
+            <font-awesome-icon icon="calendar-minus"/>
           </a>
         </div>
         <div class="control" style="width: 10vw">
